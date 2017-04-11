071298a56fafda821a8d362b82d112e630238e13
Dan Osborne <dan.osborne@rocketmail.com>
2017-04-11 09:48:49 +0100

    Work done on path builder

--- engine/sectors.js
+++ engine/sectors.js
@@ -12,12 +12,13 @@ export class sectors {
     this.grid = new grid(this.width, this.height);
     this.path = [];
     this.definePath();
-    this.fillEmptyAreas();
-  }
+    this.refinePaths();
+    // this.fillEmptyAreas();
+  };
 
-  definePath() {
-    const startX = Random(0, this.width);
-    const startY = Random(0, this.height);
+  definePath(x, y) {
+    const startX = (x) ? x : Random(0, this.width);
+    const startY = (y) ? y : Random(0, this.height);
     this.playableZone(startX, startY);
 
     let i = 0;
@@ -27,6 +28,9 @@ export class sectors {
       i ++ ;
       sect = this.availableAdjacent(this.path[i].x, this.path[i].y);
     };
+
+    this.emptyColumns = this.findEmptyAreas().emptyColumns;
+    this.emptyRows = this.findEmptyAreas().emptyRows;
   };
 
   playableZone(x, y) {
@@ -37,10 +41,26 @@ export class sectors {
   availableAdjacent(x, y) {
     let available = [];
     let choice = {};
-    if (y > 0) {available.push('n')};
-    if (y < (this.height - 1)) {available.push('s')};
-    if (x < (this.width - 1)) {available.push('e')};
-    if (x > 0) {available.push('w')};
+    if (y > 0) {
+      if (!this.grid[y - 1][x].playable) {
+        available.push('n');
+      }
+    };
+    if (y < (this.height - 1)) {
+      if (!this.grid[y + 1][x].playable) {
+        available.push('s');
+      }
+    };
+    if (x < (this.width - 1)) {
+      if (!this.grid[y][x + 1].playable) {
+        available.push('e');
+      }
+    };
+    if (x > 0) {
+      if (!this.grid[y][x - 1].playable) {
+        available.push('w');
+      }
+    };
 
     const rand = available[Random(0, available.length)];
 
@@ -71,7 +91,7 @@ export class sectors {
         this.playableZone(randomCol, rowIndex);
       });
     }
-  }
+  };
 
   findEmptyAreas() {
     const rows = {};
@@ -113,19 +133,27 @@ export class sectors {
     }
   };
 
+  refinePaths() {
+    let i = 0;
+    while (this.emptyColumns.length || this.emptyRows.length && i < 3) {
+      this.definePath();
+      i ++;
+    };
+  };
+
 }
 
-export function grid(width,height) {
+export function grid(width, height) {
   const grid = [];
-    for (let y = 0; y < height; y++) {
-      const columns = [];
-      for (let x = 0; x < width; x++) {
-          columns.push({
-            postition: {x,y},
-            playable: false
-          });
-      }
-      grid.push(columns);
+  for (let y = 0; y < height; y++) {
+    const columns = [];
+    for (let x = 0; x < width; x++) {
+        columns.push({
+          position: {x,y},
+          playable: false
+        });
     }
-    return grid;
+    grid.push(columns);
+  }
+  return grid;
 };
